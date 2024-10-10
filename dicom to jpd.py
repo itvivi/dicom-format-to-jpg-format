dba7b8099546c270a4d7b17c7cf81ede613db1dd
itvivi <121789809+itvivi@users.noreply.github.com>
2024-10-10 08:50:39 +0800

    Add files via upload
    code for converting dicom mri image to jpg

---
+++ dicom to jpd.py
@@ -0,0 +1,6 @@
+import dicom2jpg
+from dicom2jpg import dicom2jpg
+
+image_path='dcm image/MRI_IMAGE.dcm' # image path 
+
+dicom2jpg(image_path)          # convert dicom image file to jpg format    
